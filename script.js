0db6401b770c448109505154a238b846502a086c
voletisailaja <81481090+voletisailaja@users.noreply.github.com>
2021-07-19 10:43:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+
+setTimeout(()=>{
+document.querySelector("div").innerHTML=10;
+document.querySelector("div").style.color="red";
+document.querySelector("div").style.fontSize="60px";
+
+setTimeout(()=>{
+    document.querySelector("div").innerHTML=9;
+    document.querySelector("div").style.color="red";
+    document.querySelector("div").style.fontSize="60px";
+    
+    setTimeout(()=>{
+        document.querySelector("div").innerHTML=8;
+        document.querySelector("div").style.color="red";
+        document.querySelector("div").style.fontSize="60px";
+    
+        setTimeout(()=>{
+            document.querySelector("div").innerHTML=7;
+            document.querySelector("div").style.color="red";
+            document.querySelector("div").style.fontSize="60px";
+            
+            setTimeout(()=>{
+                document.querySelector("div").innerHTML=6;
+                document.querySelector("div").style.color="red";
+                document.querySelector("div").style.fontSize="60px";
+                setTimeout(()=>{
+                    document.querySelector("div").innerHTML=5;
+                    document.querySelector("div").style.color="red";
+                    document.querySelector("div").style.fontSize="60px";
+                    
+                    setTimeout(()=>{
+                        document.querySelector("div").innerHTML=4;
+                        document.querySelector("div").style.color="red";
+                        document.querySelector("div").style.fontSize="60px";
+                        
+                        setTimeout(()=>{
+                            document.querySelector("div").innerHTML=3;
+                            document.querySelector("div").style.color="red";
+                            document.querySelector("div").style.fontSize="60px";
+                            
+                            setTimeout(()=>{
+                                document.querySelector("div").innerHTML=2;
+                                document.querySelector("div").style.color="red";
+                                document.querySelector("div").style.fontSize="60px";
+                                
+                                setTimeout(()=>{
+                                    document.querySelector("div").innerHTML=1;
+                                    document.querySelector("div").style.color="red";
+                                    document.querySelector("div").style.fontSize="60px";
+                                    
+                                    setTimeout(()=>{
+                                        document.querySelector("div").innerHTML="Happy Independence Day";
+                                        document.querySelector("div").style.color="red";
+                                        document.querySelector("div").style.fontSize="60px";
+                                        
+                                        
+
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
+},1000);
+},1000);
